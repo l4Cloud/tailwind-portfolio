63358328b84bed668561726d8b15670ba95a7781
LewisC00 <lewisculleton00@gmail.com>
2022-03-24 14:12:15 +0000

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  mode: 'jit',
+  content: [
+    "./index.html",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'body' : '#17171F',
+        'body': '#17171F',
+        'selected-text': '#A3A3FF',
+        'theme': '#1472ff',
+        'nav': '#404053',
+        'secondary': '#9191A4',
+        'badge': '#3F3F51',
+        'input-border': '#565666',
+        'input': '#2A2A35'
+      },
+      fontFamily: {
+        'poppins': ["'Poppins', 'sans-serif'"]
+      }
+    },
+  },
+}
